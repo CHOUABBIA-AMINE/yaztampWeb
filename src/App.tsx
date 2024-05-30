b1d6194dd6d16d57a376fff8847e73e5c39e5b94
CHOUABBIA-AMINE <chouabbia.amine@gmail.com>
2024-05-30 19:26:04 +0100

    config tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React 								from 'react'
 
 const App = () => {
 	return (
-		<div>App</div>
+		<div className="w-full flex flex-col">App</div>
 	)
 }
 export default App;
